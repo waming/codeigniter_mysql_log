30423694111fff62266beb4edf39460986613712
waming <honghua1207@sina.com>
2015-10-16 09:27:49 +0800

    Delete conf.php

--- conf.php
+++
@@ -1,8 +0,0 @@
-'REDIS_CONF'            => array(
-                                   'master' => array(
-                                      array('host'=> '127.0.0.1', 'auth' => '', 'port' => 6379),
-                                   ),  
-                                   'slave'  => array(
-                                      array('host'=> '127.0.0.1', 'auth' => '', 'port' => 6379),
-                                   ),
-                            ), 
